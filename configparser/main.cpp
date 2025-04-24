036dd2c7b3de2a3886727c8b6cd4d5bca880d194
omartela <oskari.martela@gmail.com>
2025-04-24 14:39:32 +0300

    Adds main.cpp for testing purposes

---
+++ configparser/main.cpp
@@ -0,0 +1,19 @@
+#include "parser.hpp"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage program name + file" << std::endl;
+        return 1;
+    }
+    try
+    {
+        Parser parser(argv[1]);
+        parser.printServerConfigs();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
