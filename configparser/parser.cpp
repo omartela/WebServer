1566b4a80a75b29ff1b8ce02c45e9e7f647b04fb
omartela <oskari.martela@gmail.com>
2025-04-24 14:39:09 +0300

    Adds first iteration of the parsing logic for configuration file

--- configparser/parser.cpp
+++ configparser/parser.cpp
@@ -1,14 +1,14 @@
 #include "parser.hpp"
 
-Parser::Parser(const std::string& config_file) : config_file(config_file) 
+Parser::Parser(const std::string& config_file) 
 {
-    if (!parseConfigFile()) 
+    if (!parseConfigFile(config_file)) 
     {
         throw std::runtime_error("Failed to parse config file: " + config_file);
     }
 }
 
-Parser::parserConfigFile() 
+bool Parser::parseConfigFile(const std::string& config_file)
 {
     std::ifstream file(config_file);
     if (!file.is_open()) 
@@ -26,9 +26,9 @@ Parser::parserConfigFile()
         // Check if the line contains "server {" with a space in between
         if (line.find("server {") != std::string::npos) 
         {
+            ServerConfig server_config{}; // alustaa default arvoihin structin siksi kaarisulkeet
             while (getline(file, line) && line.find("}") == std::string::npos)
             {
-                ServerConfig server_config;
                 if (line.find("listen ") != std::string::npos)
                 {
                     // Extract host and port from the line
@@ -69,14 +69,14 @@ Parser::parserConfigFile()
                     size_t pos = line.find("client_max_body_size ") + 21; // Skip "client_max_body_size "
                     size_t end_pos = line.find(";", pos);
                     std::string size_str = line.substr(pos, end_pos - pos);
-                    if (size_str.find("k") != std::string::npos) 
+                    if (size_str.find("K") != std::string::npos) 
                     {
-                        size_str.erase(size_str.find("k"));
+                        size_str.erase(size_str.find("K"));
                         server_config.client_max_body_size = std::stoul(size_str) * 1024; // Convert to bytes
                     } 
-                    else if (size_str.find("m") != std::string::npos) 
+                    else if (size_str.find("M") != std::string::npos) 
                     {
-                        size_str.erase(size_str.find("m"));
+                        size_str.erase(size_str.find("M"));
                         server_config.client_max_body_size = std::stoul(size_str) * 1024 * 1024; // Convert to bytes
                     } 
                     else 
@@ -91,12 +91,12 @@ Parser::parserConfigFile()
                     size_t end_pos = line.find(";", pos); // need to check npos if ; not found
                     size_t space_pos = line.find(" ", pos);
                     int error_code = std::stoi(line.substr(pos, space_pos - pos));
-                    std::string error_page = line.substr(space_pos + 1, end_pos - space_pos - 1);
+                    std::string error_page = line.substr(space_pos, end_pos - space_pos);
                     server_config.error_pages[error_code] = error_page;
                 }
                 else if (line.find("location /") != std::string::npos)
                 {
-                    Route route;
+                    Route route{}; // alustaa default arvoihin struktin siksi kaarisulkeet
                     size_t pos = line.find("location /") + 10; // Skip "location /"
                     size_t end_pos = line.find("{");
                     route.path = line.substr(pos, end_pos - pos);
@@ -110,34 +110,119 @@ Parser::parserConfigFile()
                         }
                         else if (line.find("index ") != std::string::npos)
                         {
-
+                            size_t pos = line.find("index ") + 6; // Skip "index "
+                            size_t end_pos = line.find(";");
+                            route.index_file = line.substr(pos, end_pos - pos);
                         }
                         else if (line.find("autoindex ") != std::string::npos)
                         {
-                            
+                            size_t pos = line.find("autoindex ");
+                            size_t end_pos = line.find(";");
+                            std::string onoff = line.substr(pos, end_pos - pos);
+                            if (onoff.compare("off") == 0)
+                                route.autoindex == false;
+                            else if (onoff.compare("on") == 0)
+                                route.autoindex == true;
                         }
                         else if (line.find("allow_methods ") != std::string::npos)
                         {
-
+                            size_t pos = line.find("allow_methods ") + 14; // Skip "allow_methods "
+                            size_t end_pos = line.find(";");
+                            std::string methods = line.substr(pos, end_pos - pos);
+                            size_t space_pos = 0;
+                            while ((space_pos = methods.find(" ")) != std::string::npos) 
+                            {
+                                route.accepted_methods.push_back(methods.substr(0, space_pos));
+                                methods.erase(0, space_pos + 1);
+                            }
+                            route.accepted_methods.push_back(methods); // Add the last method
+                        }
+                        else if (line.find("return ") != std::string::npos)
+                        {
+                            size_t pos = line.find("return ") + 7; // Skip "return "
+                            size_t space_pos = line.find(" ", pos);
+                            route.redirect.status_code = std::stoi(line.substr(pos, space_pos - pos));
+                            route.redirect.target_url = line.substr(space_pos + 1, line.find(";") - space_pos - 1);
+                        }
+                        else if (line.find("upload_path ") != std::string::npos)
+                        {
+                            size_t pos = line.find("upload_path ") + 12; // Skip "upload_path "
+                            size_t end_pos = line.find(";");
+                            route.upload_path = line.substr(pos, end_pos - pos);
+                        }
+                        else if (line.find("cgi_extension ") != std::string::npos)
+                        {
+                            size_t pos = line.find("cgi_extension ") + 14; // Skip "cgi_extension "
+                            size_t end_pos = line.find(";");
+                            route.cgi_extension = line.substr(pos, end_pos - pos);
+                        }
+                        else if (line.find("upload_path ") != std::string::npos)
+                        {
+                            size_t pos = line.find("upload_path ") + 12; // Skip "upload_path "
+                            size_t end_pos = line.find(";");
+                            route.upload_path = line.substr(pos, end_pos - pos);
                         }
                     }
+                    server_config.routes.push_back(route);
                 }
-
-                 // Populate server_config based on the line content
-                 server_configs.push_back(serve_config);
+                // Populate server_config based on the line content
             }
+            server_configs.push_back(server_config);
         } 
-        else if (line.find("route {") != std::string::npos) 
-        {
-            Route route;
-            // Populate route based on the line content
-            server_configs.back().routes.push_back(route);
-        }
     }
-
-
     file.close();
     return true;
+}
+
+std::vector<ServerConfig> Parser::getServerConfigs() 
+{
+    return server_configs;
+}
+
+void Parser::printRoute(const Route& route) const
+{
+    std::cout << "\033[1;34mPrinting Route struct\033[0m" << std::endl;
+    std::cout << "Route Path: " << route.path << std::endl;
+    std::cout << "Root: " << route.root << std::endl;
+    std::cout << "Accepted Methods: ";
+    for (const auto& method : route.accepted_methods) 
+    {
+        std::cout << method << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Redirect status code: " << route.redirect.status_code << std::endl;
+    std::cout << "Redirect To: " << route.redirect.target_url << std::endl;
+    std::cout << "Autoindex: " << (route.autoindex ? "on" : "off") << std::endl;
+    std::cout << "Index File: " << route.index_file << std::endl;
+    std::cout << "CGI Extension: " << route.cgi_extension << std::endl;
+    std::cout << "Upload Path: " << route.upload_path << std::endl;
+
+}
+
+void Parser::printServerConfig(const ServerConfig& server_config) const
+{
+    std::cout << "\033[1;34mPrinting ServerConfig struct\033[0m" << std::endl;
+    std::cout << "Host: " << server_config.host << std::endl;
+    std::cout << "Port: " << server_config.port << std::endl;
+    std::cout << "Server Names: ";
+    for (const auto& name : server_config.server_names) 
+    {
+        std::cout << name << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Client Max Body Size: " << server_config.client_max_body_size << std::endl;
+    for (const auto& route : server_config.routes) 
+    {
+        printRoute(route);
+    }
+}
+
+void Parser::printServerConfigs() const
+{
+    for (const auto& server_config : server_configs) 
+    {
+        printServerConfig(server_config);
+    }
 }
 
 Parser::~Parser() 
--- configparser/parser.hpp
+++ configparser/parser.hpp
@@ -6,6 +6,33 @@
 #include <iostream>
 #include <fstream>
 
+// Erilaisia redirect status koodeja ja käyttötarkoituksia
+/*
+Koodi | Nimi | Käyttötarkoitus
+301 | Moved Permanently | Pysyvä uudelleenohjaus
+302 | Found (aiemmin "Moved Temporarily") | Tilapäinen uudelleenohjaus
+307 | Temporary Redirect | Kuten 302, mutta metodit säilyvät
+308 | Permanent Redirect | Kuten 301, mutta metodit säilyvät
+*/
+
+struct Redirect 
+{
+    int status_code;              // Status koodi redirectionille
+    std::string target_url;       // Esim. url uuteen osoitteeseen esim /new
+};
+
+struct Route
+{
+    std::string path;
+    std::string root;
+    std::vector<std::string> accepted_methods;
+    Redirect redirect;
+    bool autoindex;
+    std::string index_file;
+    std::string cgi_extension;
+    std::string upload_path;
+};
+
 struct ServerConfig 
 {
     std::string host;
@@ -16,32 +43,20 @@ struct ServerConfig
     std::vector<Route> routes;
 };
 
-struct Route
-{
-    std::string path;
-    std::string root;
-    std::vector<std::string> accepted_methods;
-    std::optional<std::string> redirect_to;
-    bool autoindex;
-    std::string index_file;
-    std::string cgi_extension;
-    std::string upload_path;
-};
-
 class Parser
 {
 
-private:
-    std::vector<ServerConfig> server_configs;
-public:
-    Parser(const std::string& config_file);
-    Parser(const Parser& src) = delete; // Disable copy constructor
-    Parser& operator=(const Parser& src) = delete; // Disable copy assignment operator¨
-    ~Parser();
-    void parseConfigFile(const std::string& config_file);
-    std::vector<ServerConfig> getServerConfigs();
-    void printServerConfigs() const;
-    void printRoutes() const;
-    void printRoute(const Route& route) const;
-    void printServerConfig(const ServerConfig& server_config) const;
-}
+    private:
+        std::vector<ServerConfig> server_configs;
+    public:
+        Parser(const std::string& config_file);
+        Parser(const Parser& src) = delete; // Disable copy constructor
+        Parser& operator=(const Parser& src) = delete; // Disable copy assignment operator¨
+        ~Parser();
+        bool parseConfigFile(const std::string& config_file);
+        std::vector<ServerConfig> getServerConfigs();
+        void printServerConfigs() const;
+        void printRoutes() const;
+        void printRoute(const Route& route) const;
+        void printServerConfig(const ServerConfig& server_config) const;
+};
