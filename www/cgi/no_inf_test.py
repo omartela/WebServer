1f85d657ee8d676584937b74278d96f2b24df1bb
¨Nikolai <nzharkev@student.hive.fi>
2025-06-17 10:06:57 +0300

    Saving

--- www/cgi/no_inf_test.py
+++
@@ -1,8 +0,0 @@
-import sys
-import time
-
-while True:
-    with open("test123.txt", "a") as f:
-        f.write("A")
-    time.sleep(1)
-    break
--- www/cgi/script.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import sys, os
-print("Content-Type: text/plain\n")
-print("Method:", os.environ.get("REQUEST_METHOD"))
-print("Body:", sys.stdin.read())
